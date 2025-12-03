ea0af1cab978c09f2cb2a7f236c4f59aee60fed0
tan-yun-e <tye9793@gmail.com>
2025-12-03 19:53:46 +0800

    installed primevue

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/calculator',
+      name: 'calculator',
+      component: () => import('../views/CalcView.vue'),
+    }
   ],
 })
 
---
+++ src/views/CalcView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('')
+</script>
+
+
+
+<template>
+    <div class="about">
+      <h1>This is the calculations page</h1>
+    </div>
+    <p>Message is : {{ message }}</p>
+    <input v-model="message" placeholder="wip"></input>
+  </template>
