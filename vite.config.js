4c33f69777d26de45867d45b72ed6a08413cb1e7
tan-yun-e <tye9793@gmail.com>
2025-12-06 00:59:30 +0800

    hosting on git pages

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     
   ],
+  base: "/ssw-carbon-footprint-calculator",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
