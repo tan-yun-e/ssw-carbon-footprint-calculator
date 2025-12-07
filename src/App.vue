86ee39c1b98293ae5bddfe3c8c84c5cc8099481d
tan-yun-e <tye9793@gmail.com>
2025-12-07 20:56:25 +0800

    removed extra nav bar

--- src/App.vue
+++ src/App.vue
@@ -24,43 +24,7 @@ const menuItems = [
     }
   },
 ]
-const items = ref([
-  {
-    label: 'Router',
-    icon: 'pi pi-palette',
-    items: [
-      {
-        label: 'Styled',
-        route: '/theming/styled'
-      },
-      {
-        label: 'Unstyled',
-        route: '/theming/unstyled'
-      }
-    ]
-  },
-  {
-    label: 'Programmatic',
-    icon: 'pi pi-link',
-    command: () => {
-      router.push('/introduction');
-    }
-  },
-  {
-    label: 'External',
-    icon: 'pi pi-home',
-    items: [
-      {
-        label: 'Vue.js',
-        url: 'https://vuejs.org/'
-      },
-      {
-        label: 'Vite.js',
-        url: 'https://vitejs.dev/'
-      }
-    ]
-  }
-]);
+
 </script>
 
 <template>
@@ -79,24 +43,7 @@ const items = ref([
         </a>
       </template>
   </Menubar>
-  <div class="card">
 
-    <Menubar :model="items">
-      <template #item="{ item, props, hasSubmenu }">
-        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-            <span :class="item.icon" />
-            <span>{{ item.label }}</span>
-          </a>
-        </router-link>
-        <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-          <span :class="item.icon" />
-          <span>{{ item.label }}</span>
-          <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
-        </a>
-      </template>
-    </Menubar>
-  </div>
   <RouterView />
 </template>
 
